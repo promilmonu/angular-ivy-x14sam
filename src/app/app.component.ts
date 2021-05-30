ebd4ec1950c43791a1f0fbe7a372694efdcf72f4
promilmonu <promilyahoo@gmail.com>
2021-05-30 16:19:29 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  //name = 'Angular ' + VERSION.major;
+  email: string;
+  password: string;
+  logInUser() {
+    if (this.email == 'admin@gmail.com' && this.password == '123456') {
+      console.log('welcome to dashboard User');
+    } else {
+      console.log('User Unauthorized');
+    }
+  }
 }
